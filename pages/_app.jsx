48870b13a6dae652759fdd299ba130da34fc69c0
Abu Taher Muhammad <abut1081@gmail.com>
2022-06-30 08:06:18 +0600

    Progress bar added.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,9 +2,15 @@ import "antd/dist/antd.css";
 import "../styles/error404.sass";
 import "../styles/index.sass";
 // import 'tailwindcss/tailwind.css'
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<NextNProgress />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
