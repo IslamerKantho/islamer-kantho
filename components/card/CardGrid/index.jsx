e1ff3c1ee60cabfcc662a2783b82cb9f274bf1d6
abutahermuhammad <abut1081@gmail.com>
2022-11-16 16:13:03 +0600

    `CardGrid` style fix

--- components/card/CardGrid/index.jsx
+++ components/card/CardGrid/index.jsx
@@ -8,8 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 import Image from "next/image";
-import NextLink from "next/link";
-import S from "./CardGrid.module.sass";
+import Link from "next/link";
 import { truncate } from "../../../utils/string.utils";
 import { memo } from "react";
 
@@ -17,16 +16,25 @@ const CardGrid = ({ cardData }) => {
   const article = cardData;
 
   return (
-    <Card component="article" className={S.__card} key={article.slug}>
-      <CardActionArea
-        className={S.__actionArea}
-        href={`/article/${article.slug}`}
-        component={NextLink}
-      >
-        <Box className={S.__thumbnail}>
+    <Card
+      component="article"
+      key={article.slug}
+      sx={{
+        width: "100%",
+        background: "#fff",
+        borderRadius: "5px",
+        boxShadow: "0px 0px 20px 0 rgb(0 0 0 / 15%)",
+      }}
+    >
+      <CardActionArea href={`/article/${article.slug}`} component={Link}>
+        <Box
+          sx={{
+            display: "flex",
+            borderBottom: "8px solid #055547",
+          }}
+        >
           {article.coverImage && (
             <Image
-              className={S.__img}
               src={imageBuilder(article?.coverImage)
                 .width(286)
                 .height(161)
@@ -43,14 +51,26 @@ const CardGrid = ({ cardData }) => {
           )}
         </Box>
 
-        <CardContent className={S.__content}>
-          <Chip
-            className={S.__meta}
-            label={article?.author?.name}
-            size="small"
-          />
+        <CardContent
+          sx={{
+            minHeight: "122px",
+            padding: "20px",
+            background: "#fff",
+          }}
+        >
+          <Chip label={article?.author?.name} size="small" />
 
-          <Typography variant="h3" className={S.__title} title={article.title}>
+          <Typography
+            variant="h3"
+            title={article.title}
+            sx={{
+              marginTop: "10px",
+              fontSize: "14px",
+              lineHeight: "24px",
+              fontWeight: "700",
+              color: "#055547",
+            }}
+          >
             {truncate(article.title, 50)}
           </Typography>
         </CardContent>
