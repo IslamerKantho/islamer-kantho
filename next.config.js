97ae58dfb9def065a2d7ff5762f8fc575eba685d
abutahermuhammad <abut1081@gmail.com>
2022-11-16 23:24:22 +0600

    PWA fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const nextPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const nextPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
 
 module.exports = nextPWA({
   images: {
     domains: ["cdn.sanity.io", "images.unsplash.com"],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 2592000,
   },
   compiler: {
     emotion: true,
@@ -12,12 +20,5 @@ module.exports = nextPWA({
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
-  },
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
   },
 });
