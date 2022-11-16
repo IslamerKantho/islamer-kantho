eebb2a7a9ca7144dca0e8b4281905f8614f54093
abutahermuhammad <abut1081@gmail.com>
2022-11-16 23:09:26 +0600

    PWA fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const pwa = require("next-pwa")({
-  dest: "public",
-});
+const nextPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = pwa({
+module.exports = nextPWA({
   images: {
     domains: ["cdn.sanity.io", "images.unsplash.com"],
   },
@@ -13,5 +12,12 @@ module.exports = pwa({
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
+  },
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
   },
 });
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           /> */}
+          <meta name="theme-color" content="#055547" />
+          <meta name="manifest" content="/manifest.json" />
           {this.props.emotionStyleTags}
         </Head>
 
