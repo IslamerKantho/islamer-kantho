8253e59010305117850240f7be0e5a8c633fce34
abutahermuhammad <abut1081@gmail.com>
2022-11-21 17:08:12 +0600

    fix

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -5,45 +5,52 @@ import Layout from "../../components/Layout";
 import { SIDEBAR_CATAGORY } from "../../db/categories.db";
 import { getAllPosts } from "../api/api";
 
-const PageArticles = ({ allPosts, preview }) => {
-	return (
-		<>
-			<Head>
-				<title>আরটিকেল | ইসলামের কন্ঠ</title>
-			</Head>
+const PageArticles = ({ posts, length, preview }) => {
+  return (
+    <>
+      <Head>
+        <title>আরটিকেল | ইসলামের কন্ঠ</title>
+      </Head>
 
-			<Layout preview={preview}>
-				<BlockGridPostCard key={allPosts?._id} postData={allPosts} />
-			</Layout>
-		</>
-	);
+      <Layout preview={preview}>
+        <BlockGridPostCard posts={posts} />
+      </Layout>
+    </>
+  );
 };
 
-export async function getStaticProps({ params, preview = false }) {
-	const allPostRange = [0, 12]; // Recent Article Range
-	const allPosts = await getAllPosts(preview, allPostRange);
-	return {
-		props: { allPosts, preview },
-		revalidate: 3600,
-	};
-}
-export async function getStaticPaths() {
-	const paths =
-		SIDEBAR_CATAGORY.map((cat) => ({
-			params: {
-				slug: `${cat.slug}`,
-			},
-		})) || [];
+export async function getServerSideProps(ctx) {
+  ctx.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
 
-	return {
-		paths,
-		fallback: true, // See the "fallback" section below
-	};
+  const preview = false;
+  console.log(ctx.query);
+  const allPostRange = [0, 12]; // Recent Article Range
+  const { data, length } = await getAllPosts(preview, allPostRange);
+
+  return {
+    props: { posts: data, length, preview },
+  };
 }
+// export async function getStaticPaths() {
+//   const paths =
+//     SIDEBAR_CATAGORY.map((cat) => ({
+//       params: {
+//         slug: `${cat.slug}`,
+//       },
+//     })) || [];
+
+//   return {
+//     paths,
+//     fallback: true, // See the "fallback" section below
+//   };
+// }
 
 PageArticles.propTypes = {
-	allPosts: PropsType.object,
-	preview: PropsType.bool,
+  allPosts: PropsType.object,
+  preview: PropsType.bool,
 };
 
 export default PageArticles;
