67eb6c4260ba2b1c4842dbbe4876121bdfedc0a4
abutahermuhammad <abut1081@gmail.com>
2024-04-19 11:40:57 +0600

    refactor: `footer`

--- src/components/Block/BlockFooter/index.jsx
+++ src/components/Block/BlockFooter/index.jsx
@@ -32,157 +32,44 @@ const BlockFooter = ({ className }) => {
   return (
     <>
       <Box
+        className="w-full px-0 pt-16 pb-20 bg-[#31313B] text-[#8E8E93]"
         component="footer"
-        sx={{
-          padding: "40px 0 20px",
-          width: "100%",
-          backgroundColor: "rgb(0, 0, 0)",
-          color: "rgb(142, 142, 147)",
-        }}
       >
-        {/* Footer Top */}
+
+
         <Box>
-          <Container
-            maxWidth="lg"
-            sx={{
-              display: "flex",
-              flexGrow: 1,
-              flexDirection: { xs: "column", md: "row" },
-              gap: "20px",
-              justifyContent: "space-between",
-            }}
-          >
-            {/* Logo Wrapper */}
-            <Box
-              sx={{
-                width: "50%",
-                marginBottom: { md: "80px" },
-              }}
-            >
-              <Box
-                sx={{
-                  width: { md: "270px" },
-                }}
-              >
-                <Image
+          <Container className="w-full" maxWidth="lg">
+            
+            <Box className="max-md:w-[278px] md:w-[270px]" >
+              <Image
+                  className="w-full"
                   src={`/img/branding/logo.png`}
                   alt="Islamer Kantho"
                   height={34.8}
                   width={184}
                 />
-              </Box>
             </Box>
-
-            {/* Newsletter */}
-            <Box
-              sx={{
-                width: { xs: "100%", md: "304px" },
-              }}
-            >
-              <Box
-                sx={{
-                  width: "100%",
-                }}
-              >
-                <Typography
-                  sx={{
-                    marginBottom: "22px",
-                    color: "#FFF",
-                    fontSize: "17px",
-                    lineHeight: "100%",
-                    fontWeight: 500,
-                  }}
-                >
-                  Get updates in your inbox.
-                </Typography>
-
-                <Box
-                  component="form"
-                  sx={{
-                    "& > :not(style)": { m: 1, width: "25ch" },
-                  }}
-                  noValidate
-                  autoComplete="off"
-                >
-                  <TextField
-                    placeholder="Enter your full email address"
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    color="secondary"
-                    sx={{
-                      borderWidth: "1px",
-                      borderColor: "#fff",
-                    }}
-                  />
-                </Box>
-
-                <Typography
-                  sx={{
-                    fontSize: "12px",
-                    color: "#fff",
-                    opacity: 0.5,
-                  }}
-                >
-                  You can unsubscribe at any time using the link in your emails.
-                  For more details, review our privacy policy.
-                </Typography>
-              </Box>
+            </Container>
+            
+          <Container className="w-full mt-10" maxWidth="lg">
+            <Typography className="text-xs">Scientific American is part of Springer Nature, which owns or has commercial relations with thousands of scientific publications (many of them can be found at www.springernature.com/us). Scientific American maintains a strict policy of editorial independence in reporting developments in science to our readers.</Typography>
+            
+            <Box className="mt-5">
+              {/* Footer Links */}
+              
+                {LINKS.map((item) => (
+                  <Typography
+                    key={item.title}
+                    component={Link}
+                    href={item.url}
+                    className="text-xs leading-5 font-bold mt-3 me-4 text-[#8E8E93] hover:text-secondary no-underline hover:underline"
+                  >
+                    {item.title}
+                  </Typography>
+                ))}
             </Box>
-          </Container>
-        </Box>
-
-        <Box
-          sx={{
-            marginTop: "40px",
-          }}
-        >
-          <Container
-            maxWidth="lg"
-            sx={{
-              width: "100%",
-              paddingTop: "10px",
-              display: "flex",
-              flexGrow: 1,
-              flexDirection: { xs: "column", md: "row" },
-              gap: { xs: "10px" },
-              justifyContent: { xs: "center", md: "space-between" },
-              borderTop: "1px solid #252525",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: "14px",
-              }}
-            >
-              &copy; 2020-{new Date().getFullYear()}. All right reserved.
-            </Typography>
-
-            <Box
-              sx={{
-                display: "flex",
-                gap: { xs: 1, sm: 2 },
-                justifyContent: "space-between",
-              }}
-            >
-              {LINKS.map((e, i) => (
-                <Typography
-                  key={i}
-                  href={e.url}
-                  component={Link}
-                  target="_blank"
-                  sx={{
-                    fontSize: "14px",
-                    color: "rgba(255,255,255,0.8)",
-                    "&:hover": {
-                      color: "rgb(255,255,255)",
-                    },
-                  }}
-                >
-                  {e.title}
-                </Typography>
-              ))}
-            </Box>
+            <Typography className="text-xs leading-5 font-bold mt-3">© 2020–2024. All rights reserved.</Typography>
+            <Typography className="text-xs leading-5" >Technical Support <a className="text-secondary" href="https://dot9.dev/" target="_blank" rel="opener referrer"><b>Dot9.dev</b></a></Typography>
           </Container>
         </Box>
       </Box>
